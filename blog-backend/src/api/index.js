1ffed722cd282698fd1bb3bcea197d242443dfb8
Sunhye Kwon <kwonsunhye0206@gmail.com>
2018-12-20 00:01:11 -0500

    Update index.js

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,13 +1,9 @@
+const Router = require('koa-router');
+const posts=require('./posts');
 
-const Koa=require('koa');
-const Router= require('koa-router');
-const api= require('./api');
-const app = new Koa();
-const router = new Router();
+const api= new Router();
 
-router.use('/api', api.routes());
-app.use(router.routes()).use(router.allowedMethods());
+api.use('/posts',posts.routes());
+  
 
-app.listen(4000,()=>{
-    console.log('listening to port 4000');
-});
+module.exports= api;
