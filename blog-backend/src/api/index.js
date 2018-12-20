2c79d9212bcffb8192fabecc6f9e99cf1a4767ad
Sunhye Kwon <kwonsunhye0206@gmail.com>
2018-12-19 23:55:26 -0500

    create posts route

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,8 +1,13 @@
-const Router = require('koa-router');
-const api= new Router();
 
-api.get('/test',(ctx)=>{
-    ctx.body='test success';
+const Koa=require('koa');
+const Router= require('koa-router');
+const api= require('./api');
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000,()=>{
+    console.log('listening to port 4000');
 });
-
-module.exports= api;
